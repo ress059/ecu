92117ff67b8d94eb653f16d44efa31d94018aa29
Ian Ress <ir232@cornell.edu>
2024-02-11 13:40:04 -0500

    Added fsm and event classes. Documentation is complete for fsm.h, fsm.c, and event.h

---
+++ inc/ecu/event.h
@@ -0,0 +1,118 @@
+/**
+ * @file
+ * @brief Base event class. Serves as an interface that users 
+ * inherit from to define their own events that will be dispatched 
+ * to library functions.
+ * @details Users can define their own events with application-specific
+ * data by inheriting this base event struct. For example:
+ * 
+ *     struct user_event
+ *     {
+ *         // Inherit base event class. MUST be first member.
+ *         struct event event;
+ *         
+ *         // User event data
+ *         uint8_t msg[8];
+ *         uint16_t index;
+ *     };
+ * 
+ * Library functions only use this base event class so users
+ * can upcast back to this base class when calling them. 
+ * Using the user_event struct defined earlier as an example:
+ * 
+ *     void foo(const struct event *event); // Library function.
+ * 
+ *     struct user_event my_event; 
+ * 
+ *     foo((const struct event *)&my_event);
+ * 
+ * This approach also makes it easier for any event struct to be
+ * copied over to an asynchronous queue by value. Separating out
+ * the event signal from the data would make this more difficult.
+ * I.e.
+ * 
+ *     foo(event_signal sig, const void *data);
+ * 
+ *     // vs.
+ *     struct user_event event;
+ *     foo((const void *)&event);
+ * 
+ * @author Ian Ress
+ * 
+ */
+
+
+#ifndef EVENT_BASE_CLASS_H_
+#define EVENT_BASE_CLASS_H_
+
+
+#include <stdint.h>
+
+
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------- EVENT SIGNALS --------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * @brief Template that users should follow to define their own event 
+ * signals.
+ * 
+ * @details Event signals identify the type of event that was dispatched.
+ * This template ensures there is no conflict between reserved and 
+ * user-defined event signals. Values less than @ref IDLE_EVENT are 
+ * reserved for internal use by the library. The first user-definable 
+ * signal starts at @ref USER_EVENT_BEGIN. An example user event declaration
+ * would be:
+ * 
+ *     enum user_event_signals
+ *     {
+ *         BUTTON_PRESS_EVENT = USER_EVENT_BEGIN,
+ *         TIMEOUT_EVENT,
+ *         ERROR_EVENT
+ *     };
+ */
+enum reserved_event_signals
+{
+    ENTRY_EVENT             = -3,   /**< DO NOT USE. Reserved. Run ENTRY_EVENT of state that was entered. */
+    EXIT_EVENT              = -2,   /**< DO NOT USE. Reserved. Run EXIT_EVENT of state that was exited. */
+    /****************************/
+    IDLE_EVENT              = -1,   /**< First signal available to users. Background processing event. */
+    USER_EVENT_BEGIN        = 0     /**< Start of user-defined signals. */
+};
+
+
+/**
+ * @brief Event signal. Identifies the type of event that was dispatched. 
+ * 
+ * @details This is a generic type that the library can use to implicitly
+ * typecast between this value, @ref reserved_event_signals enumeration, 
+ * and user-defined event signal enumerations. This must be a signed integer 
+ * type in order to handle @ref reserved_event_signals enumerations less than 0.
+ */
+typedef int16_t event_signal;
+
+
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------- BASE EVENT STRUCTURE ----------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * @brief Base event class. 
+ * 
+ * @details Serves as an interface that users inherit from to 
+ * define their own events that will be dispatched to library functions. 
+ * See @ref event.h for details.
+ */
+struct event
+{
+    /**
+     * @details Identifies the type of event that was dispatched.
+     * Must be greater than or equal to @ref IDLE_EVENT
+     */
+    event_signal signal;
+};
+
+
+#endif /* EVENT_BASE_CLASS_H_ */
---
+++ inc/ecu/fsm.h
@@ -0,0 +1,213 @@
+/**
+ * @file
+ * @brief Base finite state machine class. State is represented by a user-defined function.
+ * Serves as an interface that users inherit from to define their application-specific fsms.
+ * @details Users can define their own application-specific fsms by inheriting this base class.
+ * For example:
+ * 
+ *     struct user_fsm
+ *     {
+ *         // Inherit base fsm class. MUST be first member.
+ *         struct fsm fsm;
+ * 
+ *         // User fsm data
+ *         uint8_t switch_statuses;
+ *         uint16_t current_time_ms;
+ *     };
+ * 
+ * Library functions only use this base fsm class so users can upcast back to this base class 
+ * when calling them. Using the user_fsm struct defined earlier as an example:
+ * 
+ *     fsm_ctor((struct fsm *)&user_fsm); // Library function.
+ * 
+ * User-defined events and event data can be dispatched to the fsm using this same inheritance
+ * scheme. Using the same user_fsm structure:
+ * 
+ *     struct user_event
+ *     {
+ *         // Inherit base event class. MUST be first member.
+ *         struct event event;
+ *         
+ *         // User event data
+ *         uint8_t msg[8];
+ *         uint16_t index;
+ *     };
+ * 
+ *     fsm_dispatch((struct fsm *)&user_fsm, (const struct event *)&user_event);
+ * 
+ * See @ref event.h for more details on defining your own events. 
+ * 
+ * A bare-bones pseudocode example of a user-defined state would follow this template:
+ * 
+ *     enum fsm_status fsm_operational_state(struct user_fsm *fsm, struct user_event *event)
+ *     {
+ *         enum fsm_status status = FSM_EVENT_HANDLED;
+ * 
+ *         switch(event->event.signal)
+ *         {
+ *             case ENTRY_EVENT:
+ *             {
+ *                 // User logic when state first enters. Automatically ran by dispatcher.
+ *                 // DO NOT DEREFERENCE EVENT STRUCT HERE.
+ *                 break;
+ *             }
+ * 
+ *             case EXIT_EVENT:
+ *             {
+ *                 // User logic when state exits. Automatically ran by dispatcher.
+ *                 // DO NOT DEREFERENCE EVENT STRUCT HERE.
+ *                 break;
+ *             }
+ * 
+ *             case USER_EVENT_SIGNAL:
+ *             {
+ *                 if (event->data == data that causes state transition)
+ *                 {
+ *                     // If this transitions occurs dispatcher would execute: 
+ *                     // 1) This current case.
+ *                     // 2) EXIT_EVENT of this state.
+ *                     // 3) ENTRY_EVENT of "your_new_state".
+ *                     status = fsm_change_state(&your_new_state);
+ *                 }
+ *                 break;
+ *             }
+ *         }
+ * 
+ *         return status;
+ *     }
+ * 
+ * @author Ian Ress
+ * 
+ */
+
+
+#ifndef FSM_H_
+#define FSM_H_
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Runtime asserts. */
+#include "asserter.h"
+
+/* Base event class. */
+#include "event.h"
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------- FSM STATE ----------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * @brief Each fsm state handler function must return this type.
+ * 
+ * @details Dispatcher uses this information to execute state 
+ * transitions and verify fsm behavior is valid.
+ */
+enum fsm_status
+{
+    FSM_EVENT_TRANSITION,  /**< DO NOT USE. Reserved. Dispatched event caused state transition. */
+    FSM_EVENT_HANDLED,     /**< Dispatched event was handled by fsm. Stay in current state. */
+    FSM_EVENT_IGNORED,     /**< Dispatched event was ignored. Event is not relevant for current state. Stay in current state. */
+    FSM_ERROR              /**< Dispatched event or fsm behavior is invalid. Dispatcher will assert. */
+};
+
+
+/**
+ * @brief Prototype of all fsm state handler functions.
+ */
+typedef enum fsm_status (*fsm_func_ptr)(struct fsm *, const struct event *);
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------- BASE FSM CLASS ------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * @brief Base fsm class. 
+ * 
+ * @details State is represented by a function. See @ref fsm.h for details.
+ */
+struct fsm
+{
+    /**
+     * @details PRIVATE. Current state the fsm is in.
+     */
+    fsm_func_ptr state;
+
+    /**
+     * @details PRIVATE. Maximum number of consecutive state transitions 
+     * fsm can take before an assertion fires.
+     * 
+     * Used to prevent deadlock scenarios where fsm was set up incorrectly 
+     * and two states indefinitely transition between each other.
+     */
+    uint8_t max_state_transitions;
+};
+
+
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------ PUBLIC FUNCTIONS --------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * @brief Constructor.
+ * 
+ * @param fsm FSM to construct. This cannot be NULL - memory must be allocated
+ * for fsm beforehand since dynamic memory allocation is not used.
+ * @param init_state_0 Initial state. This is a function. This cannot be NULL.
+ * @param max_state_transitions_0 Maximum number of consecutive state transitions
+ * the fsm can take. This must be greater than 0.
+ */
+extern void fsm_ctor(struct fsm *fsm, fsm_func_ptr init_state_0, 
+                     uint8_t max_state_transitions_0);
+
+
+/**
+ * @brief Dispatch event to fsm.
+ * 
+ * @details Handles state transitions by automatically executing appropriate 
+ * @ref EXIT_EVENT and @ref ENTRY_EVENT cases. Verifies valid fsm behavior.
+ * 
+ * @param fsm Dispatch event to this fsm. This cannot be NULL. This must 
+ * have been previously constructed via successful constructor call before 
+ * using this function.
+ * @param event User-defined event to dispatch. This cannot be NULL. 
+ * See @ref event.h
+ */
+extern void fsm_dispatch(struct fsm *fsm, const struct event *event);
+
+
+/**
+ * @brief Transition fsm to a new state.
+ * 
+ * @details Application calls this inside their user-defined state
+ * handler functions to signal that the dispatched event caused a
+ * state transition. This function should always be used - fsm 
+ * state should never be changed manually.
+ * 
+ * @warning This cannot be called in the EXIT_EVENT case of your
+ * state.
+ * 
+ * @param fsm Change state of this fsm. This cannot be NULL. This 
+ * must have been previously constructed via successful constructor 
+ * call before using this function.
+ * @param new_state New state to transition into. This is a function.
+ * This cannot be NULL.
+ * 
+ * @return The return value is used internally so the dispatcher knows
+ * a state transition occurred.
+ */
+static inline enum fsm_status fsm_change_state(struct fsm *fsm, fsm_func_ptr new_state)
+{
+    RUNTIME_ASSERT( (fsm && new_state) );
+    fsm->state = new_state;
+    return FSM_EVENT_TRANSITION;
+}
+
+
+#endif /* FSM_H_ */
