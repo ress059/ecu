96f6cc409603b3587bf7113b5e6c4738d3aaca13
Ian Ress <ir232@cornell.edu>
2024-03-01 18:50:10 -0500

    Added coding style doc. Changed naming scheme to follow rules outlined in doc.

--- inc/ecu/asserter.h
+++ inc/ecu/asserter.h
@@ -24,14 +24,16 @@
  * when compiling. Serves as a common interface that the Application can use to
  * remain backwards compatible with various standards. Example calls:
  * 
- *     COMPILETIME_ASSERT(true, "test"); // Passes
- *     COMPILETIME_ASSERT(5==5, "test"); // Passes
+ * @code{.c}
+ * ECU_COMPILETIME_ASSERT(true, "test"); // Passes
+ * ECU_COMPILETIME_ASSERT(5==5, "test"); // Passes
  * 
- *     COMPILETIME_ASSERT(false, "test"); // Fails
- *     COMPILETIME_ASSERT(sizeof(int) >= 2, "test"); // Passes
+ * ECU_COMPILETIME_ASSERT(false, "test"); // Fails
+ * ECU_COMPILETIME_ASSERT(sizeof(int) >= 2, "test"); // Passes
+ * @endcode
  */
 /**@{*/
-#if defined(ECU_DOXYGEN_)
+#if defined(ECU_DOXYGEN)
 
     /**
      * @brief Produce compilation error if assert fails.
@@ -42,20 +44,23 @@
      * 
      * 1. Expands to this when compiling with C++11 and greater. Uses static_assert()
      * natively supported by this version.
-     * 
-     *     #define COMPILETIME_ASSERT(check_, text_)    static_assert((check_), text_)
+     * @code{.c}
+     * #define ECU_COMPILETIME_ASSERT(check_, text_)    static_assert((check_), text_)
+     * @endcode
      * 
      * 2. Expands to this when compiling with C11 and greater. Uses _Static_assert() 
      * which is defined in the assert.h header file. Therefore assert.h is also 
      * included in this case.
-     * 
-     *     #include <assert.h>
-     *     #define COMPILETIME_ASSERT(check_, text_)    _Static_assert((check_), text_)
+     * @code{.c}
+     * #include <assert.h>
+     * #define ECU_COMPILETIME_ASSERT(check_, text_)    _Static_assert((check_), text_)
+     * @endcode
      * 
      * 3. Expands to this when compiling with C23 and greater. Uses static_assert() 
      * which is natively supported in this standard.
-     * 
-     *     #define COMPILETIME_ASSERT(check_, text_)    static_assert((check_), text_)
+     * @code{.c}
+     * #define ECU_COMPILETIME_ASSERT(check_, text_)    static_assert((check_), text_)
+     * @endcode
      * 
      * 4. Expands to this when when using C/C++ standard that does not support
      * static assertions. Macro redeclares symbol to extern char compiletime_assert_[2]
@@ -63,9 +68,10 @@
      * assertion to also be used outisde of functions. Note for this case that text_
      * parameter is unused however must still be included in order to remain backwards 
      * compatible with other macro variants.
-     * 
-     *     extern char compiletime_assert_[1];
-     *     #define COMPILETIME_ASSERT(check_, text_)    extern char compiletime_assert_[((check_) ? 1 : 2)]
+     * @code{.c}
+     * extern char compiletime_assert_[1];
+     * #define ECU_COMPILETIME_ASSERT(check_, text_)    extern char compiletime_assert_[((check_) ? 1 : 2)]
+     * @endcode
      * 
      * @param check_ Condition to check. If this is true the assertion passes.
      * If this is false the assertion fails and triggers a compilation error.
@@ -74,7 +80,7 @@
      * string literal.
      * 
      */
-    #define COMPILETIME_ASSERT(check_, text_)
+    #define ECU_COMPILETIME_ASSERT(check_, text_)
 // /**@}*/ /* Compile-time Assert */
 #else
     #if defined(__cplusplus) && (__cplusplus >= 201103L)
@@ -88,7 +94,7 @@
          * @param text_ Message to print to console if assert fires. Must be a 
          * string literal.
          */
-        #define COMPILETIME_ASSERT(check_, text_)       static_assert((check_), text_)
+        #define ECU_COMPILETIME_ASSERT(check_, text_)       static_assert((check_), text_)
     #elif !defined(__cplusplus) && defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112L)
         #include <assert.h>
 
@@ -102,7 +108,7 @@
          * @param text_ Message to print to console if assert fires. Must be a 
          * string literal.
          */
-        #define COMPILETIME_ASSERT(check_, text_)       _Static_assert((check_), text_)
+        #define ECU_COMPILETIME_ASSERT(check_, text_)       _Static_assert((check_), text_)
     #elif !defined(__cplusplus) && defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202311L)
 
         /**
@@ -115,7 +121,7 @@
          * @param text_ Message to print to console if assert fires. Must be a 
          * string literal.
          */
-        #define COMPILETIME_ASSERT(check_)              static_assert((check_), "COMPILETIME_ASSERT macro fired!")
+        #define ECU_COMPILETIME_ASSERT(check_)              static_assert((check_), "COMPILETIME_ASSERT macro fired!")
     #else
         extern char compiletime_assert[1];
 
@@ -131,9 +137,9 @@
          * @param text_ Message to print to console if assert fires. Must be a 
          * string literal.
          */
-        #define COMPILETIME_ASSERT(check_)              extern char compiletime_assert[((check_) ? 1 : 2)]
+        #define ECU_COMPILETIME_ASSERT(check_)              extern char compiletime_assert[((check_) ? 1 : 2)]
     #endif
-#endif /* ECU_DOXYGEN_ */
+#endif /* ECU_DOXYGEN */
 
 
 
@@ -157,7 +163,7 @@
  * If this is false the assertion fails and triggers a compilation error.
  * This must be a literal expression that can be evaluated at compile-time.
  */
-#define RUNTIME_ASSERT(check_)                      ((check_) ? ((void)0) : (*My_Assert_Handler)())
+#define ECU_RUNTIME_ASSERT(check_)                      ((check_) ? ((void)0) : (*My_Assert_Handler)())
 
 
 /**
--- inc/ecu/event.h
+++ inc/ecu/event.h
@@ -6,44 +6,50 @@
  * @details Users can define their own events with application-specific
  * data by inheriting this base event struct. For example:
  * 
- *     struct user_event
- *     {
- *         // Inherit base event class. MUST be first member.
- *         struct event event;
+ * @code{.c}
+ * struct user_event
+ * {
+ *     // Inherit base event class. MUST be first member.
+ *     struct ecu_event event;
  *         
- *         // User event data
- *         uint8_t msg[8];
- *         uint16_t index;
- *     };
+ *     // User event data
+ *     uint8_t msg[8];
+ *     uint16_t index;
+ * };
+ * @endcode
  * 
  * Library functions only use this base event class so users
  * can upcast back to this base class when calling them. 
  * Using the user_event struct defined earlier as an example:
  * 
- *     void foo(const struct event *event); // Library function.
+ * @code{.c}
+ * void ecu_foo(const struct ecu_event *event); // Library function prototype.
  * 
- *     struct user_event my_event; 
+ * struct user_event my_event; 
  * 
- *     foo((const struct event *)&my_event);
+ * ecu_foo((const struct ecu_event *)&my_event);
+ * @endcode
  * 
  * This approach also makes it easier for any event struct to be
  * copied over to an asynchronous queue by value. Separating out
  * the event signal from the data would make this more difficult.
  * I.e.
  * 
- *     foo(event_signal sig, const void *data);
+ * @code{.c}
+ * foo(ecu_event_signal sig, const void *data);
  * 
- *     // vs.
- *     struct user_event event;
- *     foo((const void *)&event);
+ * // vs.
+ * struct user_event event;
+ * foo((const void *)&event);
+ * @endcode
  * 
  * @author Ian Ress
  * 
  */
 
 
-#ifndef EVENT_BASE_CLASS_H_
-#define EVENT_BASE_CLASS_H_
+#ifndef ECU_EVENT_H_
+#define ECU_EVENT_H_
 
 
 #include <stdint.h>
@@ -60,25 +66,26 @@
  * 
  * @details Event signals identify the type of event that was dispatched.
  * This template ensures there is no conflict between reserved and 
- * user-defined event signals. Values less than @ref IDLE_EVENT are 
+ * user-defined event signals. Values less than @ref ECU_IDLE_EVENT are 
  * reserved for internal use by the library. The first user-definable 
- * signal starts at @ref USER_EVENT_BEGIN. An example user event declaration
- * would be:
- * 
- *     enum user_event_signals
- *     {
- *         BUTTON_PRESS_EVENT = USER_EVENT_BEGIN,
- *         TIMEOUT_EVENT,
- *         ERROR_EVENT
- *     };
+ * signal starts at @ref ECU_USER_EVENT_BEGIN. An example user event 
+ * declaration would be:
+ * @code{.c}
+ * enum user_event_signals
+ * {
+ *     BUTTON_PRESS_EVENT = ECU_USER_EVENT_BEGIN,
+ *     TIMEOUT_EVENT,
+ *     ERROR_EVENT
+ * };
+ * @endcode
  */
-enum reserved_event_signals
+enum ecu_reserved_event_signals
 {
-    ENTRY_EVENT             = -3,   /**< -3. DO NOT USE. Reserved. Run ENTRY_EVENT of state that was entered. */
-    EXIT_EVENT              = -2,   /**< -2. DO NOT USE. Reserved. Run EXIT_EVENT of state that was exited. */
+    ECU_ENTRY_EVENT             = -3,   /**< -3. DO NOT USE. Reserved. Run ECU_ENTRY_EVENT of state that was entered. */
+    ECU_EXIT_EVENT              = -2,   /**< -2. DO NOT USE. Reserved. Run ECU_EXIT_EVENT of state that was exited. */
     /****************************/
-    IDLE_EVENT              = -1,   /**< -1. First signal available to users. Background processing event. */
-    USER_EVENT_BEGIN        = 0     /**< 0. Start of user-defined signals. */
+    ECU_IDLE_EVENT              = -1,   /**< -1. First signal available to users. Background processing event. */
+    ECU_USER_EVENT_BEGIN        = 0     /**< 0. Start of user-defined signals. */
 };
 
 
@@ -86,11 +93,12 @@ enum reserved_event_signals
  * @brief Event signal. Identifies the type of event that was dispatched. 
  * 
  * @details This is a generic type that the library can use to implicitly
- * typecast between this value, @ref reserved_event_signals enumeration, 
+ * typecast between this value, @ref ecu_reserved_event_signals enumeration, 
  * and user-defined event signal enumerations. This must be a signed integer 
- * type in order to handle @ref reserved_event_signals enumerations less than 0.
+ * type in order to handle @ref ecu_reserved_event_signals enumerations less 
+ * than 0.
  */
-typedef int16_t event_signal;
+typedef int16_t ecu_event_signal;
 
 
 
@@ -105,14 +113,14 @@ typedef int16_t event_signal;
  * define their own events that will be dispatched to library functions. 
  * See @ref event.h for details.
  */
-struct event
+struct ecu_event
 {
     /**
-     * @details Identifies the type of event that was dispatched.
-     * Must be greater than or equal to @ref IDLE_EVENT
+     * @brief Identifies the type of event that was dispatched.
+     * Must be greater than or equal to @ref ECU_IDLE_EVENT
      */
-    event_signal signal;
+    ecu_event_signal signal;
 };
 
 
-#endif /* EVENT_BASE_CLASS_H_ */
+#endif /* ECU_EVENT_H_ */
