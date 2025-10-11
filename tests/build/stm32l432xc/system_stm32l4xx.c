cfe55331e0dbd2d93e5e5046157fa9083c58c227
Ian Ress <ir232@cornell.edu>
2025-10-11 09:22:41 -0400

    stm32 updates:
    - Progress save of CMSIS port.

--- tests/build/stm32l432xc/system_stm32l4xx.c
+++ tests/build/stm32l432xc/system_stm32l4xx.c
@@ -196,11 +196,6 @@
 
 void SystemInit(void)
 {
-#if defined(USER_VECT_TAB_ADDRESS)
-  /* Configure the Vector Table location -------------------------------------*/
-  SCB->VTOR = VECT_TAB_BASE_ADDRESS | VECT_TAB_OFFSET;
-#endif
-
   /* FPU settings ------------------------------------------------------------*/
 #if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
   SCB->CPACR |= ((3UL << 20U)|(3UL << 22U));  /* set CP10 and CP11 Full Access */
